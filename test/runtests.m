cb85a9a69ffba42a4827deb247b1a3fa432582af
giob1994 <giob1994@gmail.com>
2018-04-25 17:44:18 +0200

    new version

---
+++ test/runtests.m
@@ -0,0 +1,10 @@
+% ##################################################
+%              ---   TEST SCRIPT  ---
+% Do not modify, unless you know what you're doing!
+% ##################################################
+
+% Load directory
+dir ..\TEST
+
+% Run the main.m file
+run('main.m')
