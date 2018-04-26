e31619e2ef2cf33662bad6628267a86731b226ad
giob1994 <giob1994@gmail.com>
2018-04-26 10:09:47 +0200

    update

--- test/run_tests.m
+++ test/run_tests.m
@@ -3,10 +3,13 @@
 % Do not modify, unless you know what you're doing!
 % ##################################################
 
+pwd()
+
 % Load directory
 dir ../test
 
-path()
+%path()
+
 
 % Run the main.m file
 run("main")
